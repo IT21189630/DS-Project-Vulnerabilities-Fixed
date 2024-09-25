53a7a1d0819c34a5e7dacb1851b28986f8ec2f92
IT21226632 <it21226632@my.sliit.lk>
2024-09-25 21:04:20 +0530

    fix static issues maleesha

--- frontend/src/components/instructor/contact-admin/ContactAdmin.js
+++ frontend/src/components/instructor/contact-admin/ContactAdmin.js
@@ -99,7 +99,7 @@ function ContactAdmin() {
 							</tr>
 							{inquiries.map((inquiry) => {
 								return (
-									<tr>
+									<tr key={inquiry._id}>
 										<td>
 											{format(
 												new Date(inquiry.createdAt),
